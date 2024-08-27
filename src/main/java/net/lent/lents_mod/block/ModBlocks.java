68d195e55d6bfee08650025e56e69707080efbbb
Jdiprizio-msu <joseph_diprizio@yahoo.com>
2024-08-27 09:39:58 -0600

    Added blocks: - Netherrack Zircon Ore - Endstone Zircon Ore
    Toned down hardness of all new blocks
    
    Will need to update so they mine faster with a pick, currently equivalent to mining by hand.

--- src/main/java/net/lent/lents_mod/block/ModBlocks.java
+++ src/main/java/net/lent/lents_mod/block/ModBlocks.java
@@ -23,21 +23,33 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, LentsMod.MOD_ID);
 
+    //added blocks
     public static final RegistryObject<Block> ZIRCON_BLOCK = registerBlock("zircon_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(6f).requiresCorrectToolForDrops()),
+                    .strength(5f).requiresCorrectToolForDrops()),
             ModCreativeModeTab.LENTS_MOD_TAB);
     public static final RegistryObject<Block> ZIRCON_ORE = registerBlock("zircon_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(6f).requiresCorrectToolForDrops(),
+                    .strength(3f).requiresCorrectToolForDrops(),
             UniformInt.of(3,7)),
             ModCreativeModeTab.LENTS_MOD_TAB);
     public static final RegistryObject<Block> DEEPSLATE_ZIRCON_ORE = registerBlock("deepslate_zircon_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
-                    .strength(6f).requiresCorrectToolForDrops(),
+                    .strength(3.5f).requiresCorrectToolForDrops(),
+                    UniformInt.of(3,7)),
+            ModCreativeModeTab.LENTS_MOD_TAB);
+    public static final RegistryObject<Block> NETHERRACK_ZIRCON_ORE = registerBlock("netherrack_zircon_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(1.5f).requiresCorrectToolForDrops(),
+                    UniformInt.of(3,7)),
+            ModCreativeModeTab.LENTS_MOD_TAB);
+    public static final RegistryObject<Block> ENDSTONE_ZIRCON_ORE = registerBlock("endstone_zircon_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(4f).requiresCorrectToolForDrops(),
                     UniformInt.of(3,7)),
             ModCreativeModeTab.LENTS_MOD_TAB);
 
+    //Helper methods
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
